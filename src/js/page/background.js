b4588d736ab7782cb42a3d89b4fe57df5bd2718d
welefen <welefen@gmail.com>
2013-07-11 19:00:38 +0800

    by welefen

---
+++ src/js/page/background.js
@@ -0,0 +1,6 @@
+/*chrome.webRequest.onBeforeRequest.addListener(function(details) {
+    var url = details.url;
+    console.log(url);
+    return {};
+   }, {urls:["<all_urls>"]}, ["blocking"] 
+)*/
